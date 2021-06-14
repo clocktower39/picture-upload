87fe99c4ff252e17acabfb7ef8b6b0c1edb52e45
clocktower39 <clocktower39@aol.com>
2021-06-14 14:50:41 -0700

    retrieve all images

--- src/User.js
+++ src/User.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
-import { Button, Grid, Input, TextField, makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Button, CardMedia, Grid, Input, TextField, makeStyles } from "@material-ui/core";
 import axios from "axios";
 
 const useStyles = makeStyles({
     root: {},
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
 })
 
 const User = () => {
     const classes = useStyles();
+    const [images, setImages] = useState([]);
   const [newUser, setNewUser] = useState({
     name: "",
     birthdate: "",
@@ -39,8 +44,18 @@ const User = () => {
     setNewUser({ ...newUser, photo: e.target.files[0] });
   };
 
+  useEffect(()=>{
+      fetch('http://192.168.56.1:5000/files').then(res => res.json()).then(data => setImages([...data]))
+      //secondary option
+  },[])
+
   return (
     <Grid className={classes.root} container xs={12} justify="center">
+        {images.map(image => (
+            <Grid key={image.filename} item xs={12}>
+                <CardMedia className={classes.media} image={`http://localhost:5000/image/${image.filename}`}/>
+            </Grid>
+        ))}
       <form onSubmit={handleSubmit} encType="multipart/form-data">
         <Grid item xs={12}>
           <Input
