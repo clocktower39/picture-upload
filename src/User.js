d0b09e928149ca51806dfa1f779ee024ff7fcf87
clocktower39 <clocktower39@aol.com>
2022-07-27 14:45:30 -0700

    material ui version update, redesign with users and images are profile pictures

--- src/User.js
+++ src/User.js
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Button, CardMedia, Grid, Input, TextField, makeStyles } from "@material-ui/core";
+import { Avatar, Button, Grid, Input, Stack, TextField, Typography } from "@mui/material";
 import axios from "axios";
 
-const useStyles = makeStyles({
-    root: {},
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-})
+const classes = {
+  root: {},
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+}
 
 const User = () => {
-    const classes = useStyles();
-    const [images, setImages] = useState([]);
+  const [userList, setUserList] = useState([]);
   const [newUser, setNewUser] = useState({
     name: "",
-    birthdate: "",
     photo: "",
   });
 
@@ -23,7 +21,7 @@ const User = () => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("file", newUser.photo);
-    formData.append("name", newUser.name);
+    formData.append("username", newUser.name);
     console.log(formData);
 
     axios
@@ -44,31 +42,29 @@ const User = () => {
     setNewUser({ ...newUser, photo: e.target.files[0] });
   };
 
-  useEffect(()=>{
-      fetch('http://192.168.56.1:5000/files').then(res => res.json()).then(data => setImages([...data]))
-      //secondary option
-  },[])
+  useEffect(() => {
+    fetch('http://192.168.56.1:5000/users').then(res => res.json()).then(data => setUserList([...data]));
+    //secondary option
+  }, [])
 
   return (
-    <Grid className={classes.root} container xs={12} justify="center">
-        {images.map(image => (
-            <Grid key={image.filename} item xs={12}>
-                <CardMedia className={classes.media} image={`http://localhost:5000/image/${image.filename}`}/>
-            </Grid>
-        ))}
+    <Grid sx={classes.root} container xs={12} justify="center">
+      <Grid container item xs={12} >
+        <Stack>
+          {userList.length > 0 && userList.map(user => (
+            <>
+              <Avatar key={user.username} src={`http://localhost:5000/image/${user.profilePicture}`} />
+              <Typography variant="caption" >Username: {user.username}</Typography>
+            </>
+          ))}
+        </Stack>
+      </Grid>
       <form onSubmit={handleSubmit} encType="multipart/form-data">
-        <Grid item xs={12}>
-          <Input
-            type="file"
-            accept=".png, .jpg, .jpeg"
-            name="photo"
-            onChange={handlePhoto}
-          />
-        </Grid>
+
         <Grid item xs={12}>
           <TextField
             type="text"
-            placeholder="name"
+            placeholder="Username"
             name="name"
             value={newUser.name}
             onChange={handleChange}
@@ -76,11 +72,11 @@ const User = () => {
         </Grid>
 
         <Grid item xs={12}>
-          <TextField
-            type="date"
-            name="birthdate"
-            value={newUser.date}
-            onChange={handleChange}
+          <Input
+            type="file"
+            accept=".png, .jpg, .jpeg"
+            name="photo"
+            onChange={handlePhoto}
           />
         </Grid>
 
